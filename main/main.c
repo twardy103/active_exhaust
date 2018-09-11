88ca377c83090147076467c762b8022125b74910
Aleksander Twardowski <twardowski.olek@gmail.com>
2018-09-11 22:44:04 +0200

    Active_exhaust: First commit with some part of main and sd_card handling.
    Signed-off-by: Aleksander Twardowski <twardowski.olek@gmail.com>

---
+++ main/main.c
@@ -0,0 +1,36 @@
+/* Active Exhaust project v1.0
+Features:
+ - SD card handling
+ - ...
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+#include "sd_card.h"
+
+void printChipInfo() {
+    /* Print chip information */
+    esp_chip_info_t chip_info;
+    esp_chip_info(&chip_info);
+    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
+            chip_info.cores,
+            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+
+    printf("silicon revision %d, ", chip_info.revision);
+
+    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
+            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+}
+
+void app_main()
+{
+	printf("############################	  Active exhaust v1.0   #################################\n");
+	printChipInfo();
+
+    printf("############################	  SD card handler       #################################\n");
+    sd_card_handler();
+}
---
+++ main/sd_card.h
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <sys/unistd.h>
+#include <sys/stat.h>
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_vfs_fat.h"
+#include "driver/sdmmc_host.h"
+#include "driver/sdspi_host.h"
+#include "sdmmc_cmd.h"
+
+static const char *TAG = "example";
+
+void sd_card_handler();
